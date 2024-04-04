db6838e83a6a3a370dab9810a75f03d298a614aa
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-04-04 23:39:34 +0300

    add class

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 
 
 class Fighter():
-    def __init__(self, name, st, hp, weapon):
+    def __init__(self, name, st, hp, weapon, money=0):
         self.name = name
         self.st = st
         self.hp = hp
         self.weapon = weapon
-
+        self.money = money
 
     def change_weapon(self, new_weapon):
         self.weapon = new_weapon
@@ -19,34 +19,67 @@ class Fighter():
         print("Вы вылечились на 20 жизней")
 
 
-class Monster():
-    def __init__(self, name, st, hp, weapon):
+class Monster(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Zombe(Monster):
+    def __init__(self, name, st, hp, weapon, cost=10):
         self.name = name
         self.st = st
         self.hp = hp
         self.weapon = weapon
+        self.cost = cost
 
+    def attack(self):
+        return f"{self.name} сильно бьёт и наносит {random.randint(1, 10)} урона."
 
 class Weapon(ABC):
     @abstractmethod
     def attack(self):
         pass
 
+
 class Sword(Weapon):
-    def __init__(self, name="меч", damage=f"{random.randint(1,8)}"):
+    def __init__(self, apr=2, name="меч", damage=f"{random.randint(1,8)}"):
         self.name = name
         self.damage = damage
+        self.apr = apr
     def attack(self):
         return f"Вы ударили и нанесли {random.randint(1,8)} урона"
 
 
 class Bow(Weapon):
-    def __init__(self, name="лук", damage=f"{random.randint(1,6)}"):
+    def __init__(self, apr=3, name="лук", damage=f"{random.randint(1,6)}"):
         self.name = name
         self.damage = damage
+        self.apr = apr
     def attack(self):
-        return f"Вы выстрелили и нанесли {random.randint(1,6)} урона"
+        return f"Вы выстрелили и нанесли {random.randint(1,8)} урона"
 
+class MonsterAttack(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+
+class MonstrSword(MonsterAttack):
+    def __init__(self, apr=2, name="меч", damage=f"{random.randint(1,6)}"):
+        self.name = name
+        self.damage = damage
+        self.apr = apr
+    def attack(self):
+        return f"{m1.name} ударил по тебе и нанёс {random.randint(1,6)} урона"
+
+
+class MonstrBow(MonsterAttack):
+    def __init__(self, apr=2, name="ker", damage=f"{random.randint(1,4)}"):
+        self.name = name
+        self.damage = damage
+        self.apr = apr
+    def attack(self):
+        return f"{m1.name} выстрелил по тебе и нанёс {random.randint(1,4)} урона"
 
 name = input("Введите имя персонажа: ")
 w = input("Введите название оружия которым хотите пользоваться(лук или меч): ")
@@ -60,37 +93,56 @@ while True:
         weap = Sword()
         break
     else:
-        w = input("Выберете оружие из предлоенного списка: меч или лук")
+        w = input("Выберете оружие из предлоенного списка: меч или лук\n")
 name_list = ["жорик", "костехрухт", "мозгоед"]
 name_weap = ["лук", "меч"]
 w1 = Fighter(name, stren, hp, weap)
 w = random.choice(name_weap)
 if w.lower() == "лук":
-    weap = Bow()
+    weap = MonstrBow()
 elif w.lower() == "меч":
-    weap = Sword()
-m1 = Monster(random.choice(name_list), stren, random.randint(10, 30), weap)
+    weap = MonstrSword()
+m1 = Zombe(random.choice(name_list), stren, random.randint(10, 30), weap)
 
-a = input("Вы зашли в подъземелье, перед Вами проход вперёд. Вы можете пойти прямо или повернуть назад. Куда вы направитесь?\n1 - прямо\n2 - уйти из подземелья\n")
+a = input("Вы зашли в подземелье, перед Вами проход вперёд. Вы можете пойти прямо или повернуть назад. Куда вы направитесь?\n1 - прямо\n2 - уйти из подземелья\n")
 if a == '1':
-    print(f"Вы решили проти дальше. Как только вы отошли от входа поальше на Вас напал {m1.name}.")
+    print(f"Вы решили проти дальше. Как только вы отошли от входа подальше на Вас напало чудовище.")
 elif a == "2":
     print("Вы как трус убежали из подъземелья и больше никогда сюда не возвращались!")
     exit()
-while m1.hp > 0:
-    b = input(f"что вы делаете?\n1 - атаковать {m1.name}а\n2 - сменить оружие на меч\n3 - сменить оружие на лук\n4 - убежать в панике\n")
-    if b == '1':
-        c = w1.weapon.attack()
-        for i in c.split():
-            if i.isdigit():
-                d = int(i)
-                m1.hp-=d
-        print(f"{c}\nу {m1.name}а осталось {m1.hp}")
-    elif b == "2":
-        w1.change_weapon(Sword())
-    elif b == "3":
-        w1.change_weapon(Bow())
-    elif b == "4":
-        print("Вы как трус убежали из подъземелья и больше никогда сюда не возвращались!")
+while True:
+    a_1 = input("Что ты будешь делать дальше?\n1 - идти дальше\n2 - забрать сокровища и уйти")
+    if a_1 == "1":
+
+        while m1.hp > 0:
+            b = input(f"что вы делаете?\n1 - атаковать {m1.name}а\n2 - сменить оружие на меч\n3 - сменить оружие на лук\n4 - убежать в панике\n")
+            if b == '1':
+                for i in range(w1.weapon.apr):
+                    c = w1.weapon.attack()
+                    for j in c.split():
+                        if j.isdigit():
+                            d = int(j)
+                            m1.hp-=d
+
+                    print(f"{c}\nу {m1.name}а осталось {m1.hp}")
+                for i in range(m1.weapon.apr):
+                    c = m1.weapon.attack()
+                    for j in c.split():
+                        if j.isdigit():
+                            d = int(j)
+                            w1.hp-=d
+
+                    print(f"{c}\nу {w1.name}а осталось {w1.hp}")
+
+            elif b == "2":
+                w1.change_weapon(Sword())
+            elif b == "3":
+                w1.change_weapon(Bow())
+            elif b == "4":
+                print("Вы как трус убежали из подземелья и больше никогда сюда не возвращались!")
+                exit()
+        print(f"Вы получили {m1.cost} серебрянных монет.")
+        w1.money+=m1.cost
+    elif a_1 == "2":
+        print(f"После победы над {m1.name}ом Вы нашли сундук с сокровищами и обрели вечную славу!")
         exit()
-print(f"После победы над {m1.name}ом Вы нашли сундук и сокровищами и обрели вечную славу!")
