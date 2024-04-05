34549e8a42a1536a4c9e202a227d3f8164c715f8
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-04-05 13:51:44 +0300

    add

--- main.py
+++ main.py
@@ -81,6 +81,48 @@ class MonstrBow(MonsterAttack):
     def attack(self):
         return f"{m1.name} выстрелил по тебе и нанёс {random.randint(1,4)} урона"
 
+def combat(m1):
+    print(f"После того как вы немного прошли вперед на вас напал {m1.name}")
+    while m1.hp > 0:
+        b = input(
+            f"что вы делаете?\n1 - атаковать {m1.name}а\n2 - сменить оружие на меч\n3 - сменить оружие на лук\n4 - убежать в панике\n")
+        if b == '1':
+            for i in range(w1.weapon.apr):
+                c = w1.weapon.attack()
+                for j in c.split():
+                    if j.isdigit():
+                        d = int(j)
+                        m1.hp -= d
+
+                print(f"{c}\nу {m1.name}а осталось {m1.hp}")
+            for i in range(m1.weapon.apr):
+                c = m1.weapon.attack()
+                for j in c.split():
+                    if j.isdigit():
+                        d = int(j)
+                        w1.hp -= d
+
+                print(f"{c}\nу {w1.name}а осталось {w1.hp}")
+
+        elif b == "2":
+            w1.change_weapon(Sword())
+        elif b == "3":
+            w1.change_weapon(Bow())
+        elif b == "4":
+            print("Вы как трус убежали из подземелья и больше никогда сюда не возвращались!")
+            exit()
+    print(f"Вы убили {m1.name}а и получили {m1.cost} серебрянных монет.")
+    w1.money += m1.cost
+
+def createMonster():
+    name_list = ["жорик", "костехрухт", "мозгоед"]
+    name_weap = ["лук", "меч"]
+    w = random.choice(name_weap)
+    if w.lower() == "лук":
+        weap = MonstrBow()
+    elif w.lower() == "меч":
+        weap = MonstrSword()
+    return Zombe(random.choice(name_list), stren, random.randint(10, 30), weap)
 name = input("Введите имя персонажа: ")
 w = input("Введите название оружия которым хотите пользоваться(лук или меч): ")
 stren = random.randint(10, 25)
@@ -94,55 +136,25 @@ while True:
         break
     else:
         w = input("Выберете оружие из предлоенного списка: меч или лук\n")
-name_list = ["жорик", "костехрухт", "мозгоед"]
-name_weap = ["лук", "меч"]
-w1 = Fighter(name, stren, hp, weap)
-w = random.choice(name_weap)
-if w.lower() == "лук":
-    weap = MonstrBow()
-elif w.lower() == "меч":
-    weap = MonstrSword()
-m1 = Zombe(random.choice(name_list), stren, random.randint(10, 30), weap)
 
-a = input("Вы зашли в подземелье, перед Вами проход вперёд. Вы можете пойти прямо или повернуть назад. Куда вы направитесь?\n1 - прямо\n2 - уйти из подземелья\n")
+w1 = Fighter(name, stren, hp, weap)
+m1 = createMonster()
+
+
+a = input("Вы стоите перед входом в подъземелье. Вы можете зайти внутрь или повернуть назад. Куда вы направитесь?\n1 - зайти\n2 - вернуться домой\n")
 if a == '1':
-    print(f"Вы решили проти дальше. Как только вы отошли от входа подальше на Вас напало чудовище.")
+    print(f"Вы решили зайти внутрь.")
 elif a == "2":
-    print("Вы как трус убежали из подъземелья и больше никогда сюда не возвращались!")
+    print("Вы как трус убежали домой!")
     exit()
 while True:
-    a_1 = input("Что ты будешь делать дальше?\n1 - идти дальше\n2 - забрать сокровища и уйти")
+    a_1 = input("Что ты будешь делать дальше?\n1 - идти дальше\n2 - уйти\n")
     if a_1 == "1":
-
-        while m1.hp > 0:
-            b = input(f"что вы делаете?\n1 - атаковать {m1.name}а\n2 - сменить оружие на меч\n3 - сменить оружие на лук\n4 - убежать в панике\n")
-            if b == '1':
-                for i in range(w1.weapon.apr):
-                    c = w1.weapon.attack()
-                    for j in c.split():
-                        if j.isdigit():
-                            d = int(j)
-                            m1.hp-=d
-
-                    print(f"{c}\nу {m1.name}а осталось {m1.hp}")
-                for i in range(m1.weapon.apr):
-                    c = m1.weapon.attack()
-                    for j in c.split():
-                        if j.isdigit():
-                            d = int(j)
-                            w1.hp-=d
-
-                    print(f"{c}\nу {w1.name}а осталось {w1.hp}")
-
-            elif b == "2":
-                w1.change_weapon(Sword())
-            elif b == "3":
-                w1.change_weapon(Bow())
-            elif b == "4":
-                print("Вы как трус убежали из подземелья и больше никогда сюда не возвращались!")
-                exit()
-        print(f"Вы получили {m1.cost} серебрянных монет.")
-        w1.money+=m1.cost
+        if m1.hp <= 0:
+            m1 = createMonster()
+            combat(m1)
+        else:
+            combat(m1)
     elif a_1 == "2":
-        print(f"После победы над {m1.name}ом Вы нашли сундук с сокровищами и обрели вечную славу!")
+        print(f"Вы вышли из подземелья!")
         exit()
